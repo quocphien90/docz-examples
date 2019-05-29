6b186f918b2d71d96eaf741b34bb4c29aea1eb00
Pham Anh Quoc Phien <quocphien90@gmail.com>
2019-05-29 16:37:23 +0700

    Updates

--- static/js/7.51177096.js
+++ static/js/7.51177096.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"./.docz/app/imports.js":function(n,e,t){"use strict";t.r(e),t.d(e,"imports",function(){return o});var o={"site/index.mdx":function(){return t.e(6).then(t.bind(null,"./site/index.mdx"))},"site/components/box.mdx":function(){return Promise.all([t.e(0),t.e(3)]).then(t.bind(null,"./site/components/box.mdx"))},"site/components/button.mdx":function(){return Promise.all([t.e(0),t.e(4)]).then(t.bind(null,"./site/components/button.mdx"))},"site/components/flex.mdx":function(){return Promise.all([t.e(0),t.e(5)]).then(t.bind(null,"./site/components/flex.mdx"))}}}},0,[0,3,4,5,6]]);
-//# sourceMappingURL=7.0a2bb168be3beb9befa3.js.map
+//# sourceMappingURL=7.b991821a4fc130ccdad5.js.map
