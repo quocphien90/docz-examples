e2bac9ed92bd5d1147e7ef4dce0751cd06d4697e
Pham Anh Quoc Phien <quocphien90@gmail.com>
2019-05-29 14:46:30 +0700

    first commit

---
+++ doczrc.js
@@ -0,0 +1,7 @@
+export default {
+    title: 'kintone UI Component',
+    description: 'An UI design language and React-based implementation',
+    dest: 'docs',
+    base: process.env.NODE_ENV === 'production' ? '/docz-examples/' : '/',
+    hashRouter: true,
+  }
