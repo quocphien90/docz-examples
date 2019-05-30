5510bf5f2149f647f9c11156ce93433e635a0255
Pham Anh Quoc Phien <quocphien90@gmail.com>
2019-05-30 12:05:08 +0700

    add redirect

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,6 +1,18 @@
 export const imports = {
-  'Button.mdx': () =>
+  'site/index.mdx': () =>
     import(
-      /* webpackPrefetch: true, webpackChunkName: "button" */ 'Button.mdx'
+      /* webpackPrefetch: true, webpackChunkName: "site-index" */ 'site/index.mdx'
+    ),
+  'site/components/box.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "site-components-box" */ 'site/components/box.mdx'
+    ),
+  'site/components/button.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "site-components-button" */ 'site/components/button.mdx'
+    ),
+  'site/components/flex.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "site-components-flex" */ 'site/components/flex.mdx'
     ),
 }
--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,10 @@
+import doczPluginNetlify from "docz-plugin-netlify";
+
 export default {
     title: 'kintone UI Component',
     description: 'An UI design language and React-based implementation',
-    dest: 'docs',
+    //dest: 'docs',
+    plugins: [doczPluginNetlify()],
     //base: process.env.NODE_ENV === 'production' ? '/' : '/',
     hashRouter: true,
     modifyBundlerConfig: bundlerConfig => {
