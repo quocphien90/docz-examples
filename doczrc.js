458c3aeff3e6aed93a8ece7c18b7e14016b179b0
Pham Anh Quoc Phien <quocphien90@gmail.com>
2019-05-30 10:05:55 +0700

    commit

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,17 @@ export default {
     title: 'kintone UI Component',
     description: 'An UI design language and React-based implementation',
     dest: 'docs',
-    base: process.env.NODE_ENV === 'production' ? '/docz-examples/' : '/',
+    //base: process.env.NODE_ENV === 'production' ? '/' : '/',
     hashRouter: true,
+    modifyBundlerConfig: bundlerConfig => {
+        const rules = [
+          {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+          }
+        ];
+        bundlerConfig.module.rules.push(...rules);
+        return bundlerConfig;
+      }
+    
   }
